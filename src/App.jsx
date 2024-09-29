f85dd1802604747689fb561a5f7dee58c7e315b4
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-09-29 19:31:43 +0630

    feat: add uncontrolled modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { UncontrolledModal } from "./components/uncontrolled-modal";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <UncontrolledModal />
+    </div>
+  );
 }
 
 export default App;
