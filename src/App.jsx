c35af20c85b31ffde8b6cd5371428ec532f82282
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-09-29 19:41:46 +0630

    feat: add controlled modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./App.css";
-import { UncontrolledModal } from "./components/uncontrolled-modal";
+import { ControlledModal } from "./components/controlled-modal";
 
 function App() {
+  const [shouldDisplay, setShouldDisplay] = useState(false);
+
   return (
     <div>
-      <UncontrolledModal />
+      <ControlledModal
+        shouldDisplay={shouldDisplay}
+        close={() => setShouldDisplay(false)}
+      >
+        <h3>I am the body of the modal</h3>
+      </ControlledModal>
+      <button onClick={() => setShouldDisplay(!shouldDisplay)}>
+        {shouldDisplay ? "Hide Modal" : "Display Modal"}
+      </button>
     </div>
   );
 }
