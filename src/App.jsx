3c957874080072769356e3d3dc437655bf969431
MyatSuMon253 <myatsumon2531999@gmail.com>
2025-02-10 20:06:08 +0630

    feat: add simple modal dialog

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import LargeAuthorListItem from "./components/layout_components/authors/LargeAuthorListItem";
-import SmallAuthorListItem from "./components/layout_components/authors/SmallAuthorListItem";
 import LargeBookListItem from "./components/layout_components/books/LargeBookListItem";
-import SmallBookListItem from "./components/layout_components/books/SmallBookListItem";
-import NumberedList from "./components/layout_components/lists/NumberedList";
-import RegularList from "./components/layout_components/lists/RegularList";
+import Modal from "./components/layout_components/Modal";
 import { authors } from "./data/authors";
 import { books } from "./data/books";
 
@@ -11,27 +7,9 @@ const App = () => {
   console.log(authors);
   return (
     <>
-      <NumberedList
-        items={authors}
-        sourceName={"author"}
-        ItemComponent={SmallAuthorListItem}
-      />
-      <RegularList
-        items={authors}
-        sourceName={"author"}
-        ItemComponent={LargeAuthorListItem}
-      />
-
-      <NumberedList
-        items={books}
-        sourceName={"book"}
-        ItemComponent={SmallBookListItem}
-      />
-      <RegularList
-        items={books}
-        sourceName={"book"}
-        ItemComponent={LargeBookListItem}
-      />
+      <Modal>
+        <LargeBookListItem book={books[1]} />
+      </Modal>
     </>
   );
 };
